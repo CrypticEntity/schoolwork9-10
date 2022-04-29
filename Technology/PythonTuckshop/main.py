16aef8990cb438866038f3fcd644ea68617978bc
CrypticEntity <jemasthebeast@gmail.com>
2022-04-29 13:29:56 +1000

    update

---
+++ Technology/PythonTuckshop/main.py
@@ -0,0 +1,9 @@
+money = 0
+addMoneyAmount = 0
+
+while money == 0:
+    responseToPoorness = input("you don't have any money, you would like to add some.")
+    if responseToPoorness == "yes":
+        addMoneyAmount = input(int("How Much?"))
+        money + addMoneyAmount = money
+
