1d670ea47024de4da695ebfa96adb6e899d44a09
CrypticEntity <jemasthebeast@gmail.com>
2022-05-03 12:29:32 +1000

    update

--- Technology/PythonTuckshop/main.py
+++ Technology/PythonTuckshop/main.py
@@ -1,9 +1,46 @@
 money = 0
 addMoneyAmount = 0
+menuResponse = 0
 
-while money == 0:
-    responseToPoorness = input("you don't have any money, you would like to add some.")
-    if responseToPoorness == "yes":
-        addMoneyAmount = input(int("How Much?"))
-        money + addMoneyAmount = money
+
+def addMoney():
+    if money == 0:        
+        addMoneyConfirm = input("you don't have any money, you would like to add some:")
+    else:
+        addMoneyConfirm = input("You Have $", money, " You would like to add more")
+    
+    if addMoneyConfirm == "yes":
+        addMoneyAmount = int(input("How Much:"))
+        money = money + addMoneyAmount
+        main()
+    
+    else:
+        print("did not understand, type yes or no")
+        returnToMain = input("Would you like to return to main menu")
+        if returnToMain == "no":
+            addMoney()
+        else:
+            main()
+        
+
+
+def main():
+    print("Welcome to the Tuckshop, you would like by something:")
+    print("**********************")
+    print("Type number to proceed")
+    print("1: Buy Somethin")
+    print("2: Add to Balence")
+    menuReponse = input("Option:")
+    
+    if menuReponse == 1:
+        buyMenu()
+    if menuResponse == 2:
+        addMoney()
+
+
+
+
+addMoney()
+main()
+
 
