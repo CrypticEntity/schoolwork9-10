fef7c4987e3c293804458ade01f9359739a020eb
CrypticEntity <jemasthebeast@gmail.com>
2022-05-09 09:30:56 +1000

    update

--- Technology/PythonTuckshop/main.py
+++ Technology/PythonTuckshop/main.py
@@ -1,20 +1,41 @@
-def addMoney():
+chickenStripCost = 150
+juiceCost = 300
+milkCost = 300
+pastyCost = 350
+toastyCost = 400
+waterCost = 200
+wrapCost = 450
+
+
+def sureChecker(): 
+    sureCheck = input("Are you sure:")
+    if sureCheck == "yes":
+        return True
+    else:
+        return False
+                
+
+def updateMoneyVariable():
     moneyFile = open("money", "r")
     money = int(moneyFile.readline())
     moneyFile.close()
-    
+    return money
+
+
+def addMoney():
+    money = updateMoneyVariable()
+
     if money == 0:        
         addMoneyConfirm = input("you don't have any money, you would like to add some:")
 
     else:    
-        addMoneyConfirm = input("You Have $"+ str(money)+ " You would like to add more:")
+        addMoneyConfirm = input("You Have $"+ str(money / 100)+ " You would like to add more:")
         if addMoneyConfirm == "no":
             main()
 
     if addMoneyConfirm == "yes":
-        addMoneyAmount = int(input("How Much:"))
-        money = money + addMoneyAmount
-        
+        addMoneyAmount = int(input("How Much $:"))
+        money += addMoneyAmount * 100       
         moneyFile = open("money", "w")
         moneyFile.writelines(str(money))
         moneyFile.close()
@@ -29,28 +50,58 @@ def addMoney():
             main()
         
 def buySomething():
+    firstTimeRun = 0
+    if firstTimeRun <= 0:
+        orderTotal = 0
+    firstTimeRun += 1
+
+    money = updateMoneyVariable()
+    print("You Have $"+str(money / 100))
     print("What would you like to buy?")
-     
+    print("Type number to proceed")
+    print("1: Food")
+    print("2: Drink")
+    menuResponse = int(input("Option:"))
+    print("\n\n")
+    if menuResponse == 1:
+        print("What Food do You Want")
+        print("Type Number To Proceed")
+        print("1: Chicken Strip, $1.50")
+        print("2: Chichen Wrap, $4.50")
+        print("3: Pasty, $3.50")
+        print("4: Toasty, $4.00")
+        menuResponse = input("Option:")
+        if menuResponse == 1:
+            if sureChecker() == True:
+                orderTotal += chickenStripCost
+
+
+    if menuResponse == 2: 
+        print("What Drink do You Want")
+        print("Type Number To Proceed")
+        print("1: Milk, $3.00")
+        print("2: Juice, $3.00")
+        print("3: Water, $2.00")
+        menuResponse = input("Option:")
+
+
 
 def main():
-
-    print("\n \n \n")
+    print("\n\n\n")
     print("Welcome to the Tuckshop")
     print("**********************")
     print("Type number to proceed")
     print("1: Buy Somethin")
     print("2: Add to Balence")
-    menuResponse = input("Option:")
+    menuResponse = int(input("Option:"))
+    print("\n\n\n")
     
-    print("\n \n \n")
-    if menuResponse == 2:
-        addMoney()
     if menuResponse == 1:
         buySomething()
+    if menuResponse == 2:
+        addMoney()
 
 
 
-addMoney()
-main()
-
-
+if __name__ == '__main__':
+    addMoney()
