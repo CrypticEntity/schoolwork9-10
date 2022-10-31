e88cdffa85624173b31e43d9d51e6ac36276c31a
CrypticEntity <jemasthebeast@gmail.com>
2022-10-31 21:50:54 +1000

    upate

--- Technology/fxsdk/CFun/src/main.c
+++ Technology/fxsdk/CFun/src/main.c
@@ -1,10 +1,12 @@
 #include <gint/display.h>
 #include <gint/keyboard.h>
+#include <fxlibc/printf.h>
 
 int main(void)
 {
+	font_t const *old_font = dfont(new_font);
 	dclear(C_WHITE);
-	dtext(1, 1, C_BLACK, "Sample fxSDK add-in.");
+	dtext(1, 1, C_BLACK, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
 	dupdate();
 
 	getkey();
