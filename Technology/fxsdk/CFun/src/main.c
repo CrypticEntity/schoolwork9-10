ca9c4d519ea9adb6b1e662e4a3c9cd1fba27923c
CrypticEntity <jemasthebeast@gmail.com>
2022-11-01 15:45:13 +1000

    upate

--- Technology/fxsdk/CFun/src/main.c
+++ Technology/fxsdk/CFun/src/main.c
@@ -4,9 +4,8 @@
 
 int main(void)
 {
-	font_t const *old_font = dfont(new_font);
 	dclear(C_WHITE);
-	dtext(1, 1, C_BLACK, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+	dtext(1, 1, C_BLACK, "@");
 	dupdate();
 
 	getkey();
